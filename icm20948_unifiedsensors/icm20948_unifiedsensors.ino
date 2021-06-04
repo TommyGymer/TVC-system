021aa47a5df0405f839cfdfc8348f5b1a6218178
TommyGymer <tommygymer@gmail.com>
2021-06-04 14:53:56 +0100

    Comment updates
    Updates to the comments
    New explanations added to code

--- icm20948_unifiedsensors/icm20948_unifiedsensors.ino
+++ icm20948_unifiedsensors/icm20948_unifiedsensors.ino
@@ -11,64 +11,59 @@ Adafruit_Sensor *icm_temp, *icm_accel, *icm_gyro, *icm_mag;
 #define ICM_MISO 12
 #define ICM_MOSI 11
 
-float gyro_vx, gyro_vy, gyro_vz;
-float gyro_dx, gyro_dy, gyro_dz;
-float gyro_off_x, gyro_off_y, gyro_off_z;
-unsigned long currentTime, previousTime;
-float elapsedTime;
-uint8_t gyro_divisor;
+float gyro_vx, gyro_vy, gyro_vz;                                    //gyro values
+float gyro_off_x, gyro_off_y, gyro_off_z;                           //gyro offsets
+unsigned long currentTime, previousTime;                            //to calculate change in time
+float elapsedTime;                                                  //calculated change in time
+uint8_t gyro_divisor;                                               //divider to convert gyro data to useful information
 
 //
 
 void setup(void) {
-  Serial.begin(115200);
-  while (!Serial)
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
-
-  //Serial.println("Adafruit ICM20948 test!");
-
-  if (!icm.begin_I2C()) {
-    // if (!icm.begin_SPI(ICM_CS)) {
+  Serial.begin(115200);                                             //start the serial connection at 115200 baud
+  while (!Serial)                                                   //wait for a serial connection
+  delay(10);                                                        //pause to ensure connection
+  
+  if (!icm.begin_I2C()) {                                           //check if connected to the IC over I2C
+    // if (!icm.begin_SPI(ICM_CS)) {                                //unused SPI connectors
     // if (!icm.begin_SPI(ICM_CS, ICM_SCK, ICM_MISO, ICM_MOSI)) {
     Serial.println("Failed to find ICM20948 chip");
     while (1) {
-      delay(10);
+      delay(10);                                                    //if not connected, wait
     }
   }
 
-  //Serial.println("ICM20948 Found!");
-  icm_temp = icm.getTemperatureSensor();
-  icm_temp->printSensorDetails();
+  //get sensors and print out sensor details
+  icm_temp = icm.getTemperatureSensor();                            //get the temperature sensor
+  icm_temp->printSensorDetails();                                   //print the sensor details
 
-  icm_accel = icm.getAccelerometerSensor();
-  icm_accel->printSensorDetails();
+  icm_accel = icm.getAccelerometerSensor();                         //get the accelerometer
+  icm_accel->printSensorDetails();                                  //print the sensor details
 
-  icm_gyro = icm.getGyroSensor();
-  icm_gyro->printSensorDetails();
+  icm_gyro = icm.getGyroSensor();                                   //get the gyroscope
+  icm_gyro->printSensorDetails();                                   //print the sensor details
 
-  icm_mag = icm.getMagnetometerSensor();
-  icm_mag->printSensorDetails();
+  icm_mag = icm.getMagnetometerSensor();                            //get the magnetometer
+  icm_mag->printSensorDetails();                                    //print the sensor details
 
-  gyro_divisor = icm.getGyroRateDivisor();
+  gyro_divisor = icm.getGyroRateDivisor();                          //get the divisor for the gyro data
 
+  //initialise the gyro values to 0
   gyro_vx = 0;
   gyro_vy = 0;
   gyro_vz = 0;
-
-  gyro_dx = 0;
-  gyro_dy = 0;
-  gyro_dz = 0;
-
+  
   gyro_off_x = 0;
   gyro_off_y = 0;
   gyro_off_z = 0;
 
+  //initialise values to calculate the first degree inherent offset
   long gyro_sum_x, gyro_sum_y, gyro_sum_z;
   gyro_sum_x = 0;
   gyro_sum_y = 0;
   gyro_sum_z = 0;
 
-  //find offsets
+  //loop and sum measurements
   int repeats = 10000;
   int i = 1;
   while(i < repeats){
@@ -82,6 +77,7 @@ void setup(void) {
     i++;
   }
 
+  //divide the summed measurements by the number of measurements to calculate an average offset from zero
   gyro_off_x = gyro_sum_x / i;
   gyro_off_y = gyro_sum_y / i;
   gyro_off_z = gyro_sum_z / i;
@@ -90,7 +86,7 @@ void setup(void) {
 }
 
 void loop() {
-  //  /* Get a new normalized sensor event */
+  //get the next measurement events from each of the sensors
   sensors_event_t accel;
   sensors_event_t gyro;
   sensors_event_t temp;
@@ -100,43 +96,11 @@ void loop() {
   icm_gyro->getEvent(&gyro);
   icm_mag->getEvent(&mag);
 
-  previousTime = currentTime; // Previous time is stored before the actual time read
-  currentTime = micros(); // Current time actual time read
-  elapsedTime = (currentTime - previousTime) / 1000000.0; // Divide by 1000 to get seconds
+  previousTime = currentTime;                               // Previous time is stored before the actual time read
+  currentTime = micros();                                   // Current time actual time read
+  elapsedTime = (currentTime - previousTime) / 1000000.0;   // Divide by 1000000 to get seconds
 
-//  Serial.print("\t\tTemperature ");
-//  Serial.print(temp.temperature);
-//  Serial.println(" deg C");
-//
-//  /* Display the results (acceleration is measured in m/s^2) */
-//  Serial.print("\t\tAccel X: ");
-//  Serial.print(accel.acceleration.x);
-//  Serial.print(" \tY: ");
-//  Serial.print(accel.acceleration.y);
-//  Serial.print(" \tZ: ");
-//  Serial.print(accel.acceleration.z);
-//  Serial.println(" m/s^2 ");
-//
-//  /* Display the results (rotation is measured in rad/s) */
-//  Serial.print("\t\tGyro X: ");
-//  Serial.print(gyro.gyro.x);
-//  Serial.print(" \tY: ");
-//  Serial.print(gyro.gyro.y);
-//  Serial.print(" \tZ: ");
-//  Serial.print(gyro.gyro.z);
-//  Serial.println(" radians/s ");
-//  Serial.println();
-//
-//  Serial.print("\t\tMag X: ");
-//  Serial.print(mag.magnetic.x);
-//  Serial.print(" \tY: ");
-//  Serial.print(mag.magnetic.y);
-//  Serial.print(" \tZ: ");
-//  Serial.print(mag.magnetic.z);
-//  Serial.println(" uT");
-//  delay(100);
-
-  //   serial plotter friendly format
+  //serial plotter friendly format
   Serial.print("data:");
   Serial.print(temp.temperature);
   Serial.print(",");
@@ -146,31 +110,16 @@ void loop() {
   Serial.print(","); Serial.print(accel.acceleration.z);
   Serial.print(",");
 
-  //this is the rotational acceleration rather than the rotational displacement
+  //gyro.gyro.* are the gyro velocities
   gyro_vx = gyro_vx + ((gyro.gyro.x / gyro_divisor - gyro_off_x) * elapsedTime);
   gyro_vy = gyro_vy + ((gyro.gyro.y / gyro_divisor - gyro_off_y) * elapsedTime);
   gyro_vz = gyro_vz + ((gyro.gyro.z / gyro_divisor - gyro_off_z) * elapsedTime);
 
-  //rotational velocity to rotational displacement
-  gyro_dx = gyro_dx + (gyro_vx * elapsedTime);
-  gyro_dy = gyro_dy + (gyro_vy * elapsedTime);
-  gyro_dz = gyro_dz + (gyro_vz * elapsedTime);
-
-  /*
-  Serial.print(gyro_dx);
-  Serial.print(","); Serial.print(gyro_dy);
-  Serial.print(","); Serial.print(gyro_dz);
-  */
    /**/
   Serial.print(gyro_vx * 1000);
   Serial.print(","); Serial.print(gyro_vy * 1000);
   Serial.print(","); Serial.print(gyro_vz * 1000);
   /**/
-  /*
-  Serial.print(gyro.gyro.x);
-  Serial.print(","); Serial.print(gyro.gyro.y);
-  Serial.print(","); Serial.print(gyro.gyro.z);
-  */
 
   Serial.print(",");
   Serial.print(mag.magnetic.x);
@@ -178,6 +127,6 @@ void loop() {
   Serial.print(","); Serial.print(mag.magnetic.z);
 
   Serial.println();
-  delay(100);
+  delay(10);
   
 }
