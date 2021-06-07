a784d5bacb49263bba8b4f23f496c5ec72ed34c1
TommyGymer <tommygymer@gmail.com>
2021-06-07 23:42:50 +0100

    Servo limit testing
    Testing the limits of the servo to find the minimum and maximum value that can be input into the Arduino servo library without straining the servo
    Also finding the relation between the values given to the servo and the physical angle turned by the servo

--- servos/servo_test/servo_test.ino
+++ servos/servo_test/servo_test.ino
@@ -1,20 +1,21 @@
 #include <Servo.h>
 
-Servo servo;
+Servo servo;                                                        //min: 15   max: 180  range: PI rad
 int pin = 12;
 int pos = 0;
 
 void setup() {
+  Serial.begin(115200);                                             //start the serial connection at 115200 baud
+  while (!Serial)                                                   //wait for a serial connection
+  Serial.println("Serial connected");
+  delay(10);                                                        //pause to ensure connection
   servo.attach(pin);
 }
 
 void loop() {
-  for(pos = 0; pos <= 180; pos += 1){
-    servo.write(pos);
-    delay(20);
-  }
-  for(pos = 180; pos >= 0; pos -= 1){
-    servo.write(pos);
-    delay(20);
+  if(Serial.available() > 2){
+    int val = (int) Serial.parseInt();
+    Serial.println(val);
+    servo.write(val);
   }
 }
