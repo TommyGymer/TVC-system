238b7c66930c85bf687588aa71c7037af7045a70
TommyGymer <tommygymer@gmail.com>
2021-06-08 00:16:12 +0100

    .h complete
    Created the .h file for the lib

---
+++ servos/PServo.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include <Servo.h>
+#include "PServo.h"
+
+void connect(int pin)
+{
+
+}
+
+void diconnect(void)
+{
+
+}
+
+void write(int val)
+{
+
+}
+
+void writeMicro(int val)
+{
+
+}
+
+void writeAngle(float angle)
+{
+	
+}
---
+++ servos/PServo.h
@@ -0,0 +1,29 @@
+/*
+	A servo library for causing an angle change
+*/
+
+#ifndef PServo_h
+#define PServo_h
+
+#include <Arduino.h>
+#include <Servo.h>
+
+class PServo
+{
+	public:
+		PServo();
+		PServo(int min, int max) : _min(min), _max(max) {}
+		PServo(int min, int max, float range) : _min(min), _max(max), _range(range) {}
+		void connect(int pin);
+		void diconnect(void);
+		void write(int val);
+		void writeMicro(int val);
+		void writeAngle(float angle);
+	private:
+		int _pin{12};
+		int _min{15};
+		int _max{180};
+		float _range{PI};
+}
+
+#endif
